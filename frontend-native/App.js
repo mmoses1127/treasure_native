3460cac844d2d9f863b01f5ac01e15e92bc34d7c
Daniel Culbertson <daniel.j.culbertson@gmail.com>
2023-02-16 12:43:31 -0800

    provider

--- frontend-native/App.js
+++ frontend-native/App.js
@@ -4,16 +4,20 @@ import { NativeRouter, Routes, Route, Redirect } from 'react-router-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainPage from './screens/MainPage/MainPage';
+import configureStore from './store/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
-
+  const store = configureStore();
   const Stack = createNativeStackNavigator();
   return (
+  <Provider store={store}>
    <NavigationContainer>
       <Stack.Navigator initialRouteName='MainPage'>
       <Stack.Screen name="MainPage" component={MainPage} />
       </Stack.Navigator>
    </NavigationContainer>
+   </Provider>
   );
 }
 
